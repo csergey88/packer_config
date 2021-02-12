000f1a1ec42eecaa65af5c8a53d0e2ea2150963b
csergey88 <s.chornyy8@gmail.com>
2021-02-12 18:32:30 +0200

    commit

---
+++ scripts/install_mongodb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Install MongoDB
+apt install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt update
+apt install -y mongodb-org
+systemctl enable mongod
